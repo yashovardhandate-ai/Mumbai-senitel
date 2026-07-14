89ee2f625983f2c15349963f209f7cd2b577f408
Yashovardhan Date <yashovardhandate@gmail.com>
2026-07-14 22:37:25 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE = "sentinel-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
